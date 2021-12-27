01f64f5b0607829befd4d28f9dd3c004779083aa
Kinsly <59818978+D0ubeQ@users.noreply.github.com>
2021-12-27 14:22:19 +0800

    BLL files

---
+++ BLL/Encrypt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace BLL
+{
+    public class Encrypt
+    {
+        public static string EncryptWithMD5(string source)
+        {
+            byte[] sor = Encoding.UTF8.GetBytes(source);
+            MD5 md5 = MD5.Create();
+            byte[] result = md5.ComputeHash(sor);
+            StringBuilder strbul = new StringBuilder(40);
+            for (int i = 0; i < result.Length; i++)
+            {
+                strbul.Append(result[i].ToString("x2"));//加密结果"x2"结果为32位,"x3"结果为48位,"x4"结果为64位
+            }
+            return strbul.ToString();
+        }
+    }
+}
