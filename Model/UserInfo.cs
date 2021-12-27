4185af940cbe0faa2cec1327c194cc4dae590aad
Kinsly <59818978+D0ubeQ@users.noreply.github.com>
2021-12-27 14:22:44 +0800

    Model files

---
+++ Model/UserInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+        public string UserType { get; set; }
+        public string Avatar { get; set; }
+        public string Gender { get; set; }
+        public string Mobile { get; set; }
+    }
+    public class Goods
+    {
+        public Guid Id { get; set; }
+        public string GoodsName { get; set; }
+        public string  MerId { get; set; }
+        public string Descript { get; set; }
+        public string GoodsPic { get; set; }
+        public string  Price { get; set; }
+        public string  Stock { get; set; }
+
+    }
+    public class Order
+    {
+
+    }
+}
